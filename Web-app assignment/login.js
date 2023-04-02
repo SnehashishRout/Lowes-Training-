947fdc22c990c0b3fb7af9c04b57b3271b9efc62
SnehashishRout <snehashishrout@gmail.com>
2023-04-02 14:15:22 +0530

    Web assessment

---
+++ Web-app assignment/login.js
@@ -0,0 +1,7 @@
+document.querySelector(".signIn_btn").addEventListener("click",function () {
+    location.href = "product.html";
+});
+
+document.querySelector(".signUp_btn").addEventListener("click",function () {
+    location.href = "signUP.html";
+});
